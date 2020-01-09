82e28a78221246a8ac7efa8fa5b4bedc2f3bf75d
don11995 <personshelldon@gmail.com>
2020-01-09 16:08:04 +0200

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Media;
+using System.Windows;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace SilentApp
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>private NotifyIcon trayIcon;
+    /// 
+
+    public interface IOnPlayFinishedListener {
+        void OnPlayFinished();
+    }
+
+    public class PlayerWork
+    {
+        private readonly IOnPlayFinishedListener listener;
+
+        public PlayerWork(IOnPlayFinishedListener listener) {
+            this.listener = listener;
+        }
+
+        public void PlaySound() {
+            GC.Collect();
+            SoundPlayer soundPlayer = new SoundPlayer(SilentApp.Properties.Resources.silence);
+            soundPlayer.PlaySync();
+            soundPlayer.Stop();
+            listener.OnPlayFinished();
+        }   
+    }
+
+    public partial class App : System.Windows.Application, IOnPlayFinishedListener
+    {
+        private NotifyIcon trayIcon;
+
+        protected override void OnStartup(StartupEventArgs e) {
+            OnLoad();
+            PlaySound();
+            AddApplicationToStartup();
+        }
+
+        public void OnPlayFinished() {
+            PlaySound();
+        }
+
+        private void OnLoad()
+        {
+            // Initialize Tray Icon
+            trayIcon = new NotifyIcon()
+            {
+                Icon = SilentApp.Properties.Resources.AppIcon,
+                Text = "Silent App",
+                ContextMenu = new ContextMenu(
+                    new MenuItem[] {
+                    new MenuItem("Exit", CloseApp)
+                }),
+                Visible = true
+            };
+        }
+
+        private void PlaySound()
+        {
+            PlayerWork playerWork = new PlayerWork(this);
+            Thread t = new Thread(playerWork.PlaySound);
+            t.Start();
+        }
+
+        private void CloseApp(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            trayIcon.Visible = false;
+            Environment.Exit(0);
+        }
+
+        public void AddApplicationToStartup()
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+            {
+                key.SetValue("Silent App", "\"" + System.Reflection.Assembly.GetExecutingAssembly().Location + "\"");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# SilentApp
+![](AppIcon.ico)
+
+Application that helps to workaround problem with some Samsung sound bars (Samsung HW-K450 in my case). This sound bar has auto turn off function on HDMI port if there is no any activity (sound) on it. This fucntion can not be disabled on sound bar itself (auto turn off can be disabled only on AUX port).
+
+This application plays empty sound in a cycle and therefore sound bar sees activity and does not turn off.
+
+### Instructions
+- Download and compile in **Visual Studio**
+- Run **SilentApp.exe**
+- Done!
+
+Application starts in a tray and adds itself to autorun on Windows boot. App has no any interface.
